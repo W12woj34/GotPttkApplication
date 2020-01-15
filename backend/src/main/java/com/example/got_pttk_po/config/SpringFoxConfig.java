c1dc90687b4013212389a70ed0349f8d4226f543
W12woj34 <woj120@gmail.com>
2020-01-15 21:31:51 +0100

    refactor swagger annotation

--- backend/src/main/java/com/example/got_pttk_po/config/SpringFoxConfig.java
+++ backend/src/main/java/com/example/got_pttk_po/config/SpringFoxConfig.java
@@ -1,17 +1,17 @@
 package com.example.got_pttk_po.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
     @Bean
